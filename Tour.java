7916933460986b0c2dabf765ca3102e8fbf58b75
synchi64 <y.moussan@gmail.com>
2017-05-03 15:05:02 +0200

    Update Tour.java
    what's funny about... BIGGUS... DICKUS?

--- Tour.java
+++ Tour.java
@@ -1,17 +1,30 @@
 package echec;
 
-public class Tour  extends Piece{
+public class Tour extends Piece{
 
 	public Tour(boolean noir, Coordonees c) {
 		super(noir, c);
 	}
 
-	public boolean deplPossible(Coordonees c){
+	public boolean deplPossible(PlateauDeJeu j,Coordonees c){
+
 		if(this.getCoordonnees().getAbcisse()!=c.abcisse && this.getCoordonnees().getOrdonnee()==c.ordonnee || this.getCoordonnees().getAbcisse()==c.abcisse && this.getCoordonnees().getOrdonnee()!=c.ordonnee){
-			return true;
+			if(j.getCase(c)==null){
+				return true;
+			}
+			
+			else if(j.getCase(c)!=null){
+				if(j.getCase(c).estNoir==this.estNoir){
+					return false;
+				}
+				else{
+					return true;
+				}
+			}
 		}
 		else{
 			return false;
 		}
+		
 	}
 }

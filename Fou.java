b778c8e142bcdd354e6cffbf0ee6da19ed7c828d
synchi64 <y.moussan@gmail.com>
2017-05-06 22:51:58 +0200

    Create Fou.java
    lol

---
+++ Fou.java
@@ -0,0 +1,15 @@
+package echec;
+
+public class Fou extends Piece{
+
+	public Fou(boolean noir, Coordonnees c) {
+		super(noir, c);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean deplPossible(Coordonnees c, PlateauDeJeu p) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}

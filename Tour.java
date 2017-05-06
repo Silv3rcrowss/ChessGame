59e28fd856ad9d97f850604848185f7611734fc1
synchi64 <y.moussan@gmail.com>
2017-05-06 23:33:27 +0200

    Update Tour.java
    lol

--- Tour.java
+++ Tour.java
@@ -1,28 +1,53 @@
+package echec;
+
 public class Tour extends Piece{
 
 	public Tour(boolean noir, Coordonnees c) {
 		super(noir, c);
 	}
 
-	public boolean deplPossible(Coordonnees c,PlateauDeJeu j){
+	public boolean deplPossible(Coordonnees c, PlateauDeJeu p){
 
-		if(this.getCoordonnees().getAbcisse()!=c.getAbcisse() && this.getCoordonnees().getOrdonnee()==c.getOrdonnee() || this.getCoordonnees().getAbcisse()==c.getAbcisse() && this.getCoordonnees().getOrdonnee()!=c.getOrdonnee()){
-			if(j.getCase(c)==null){
-				return true;
-			}
+		int i=0;
+		boolean flag=true;
+
+		if(this.getCoordonnees().getAbcisse()!=c.getAbcisse() && this.getCoordonnees().getOrdonnee()==c.getOrdonnee()){
 			
-			else if(j.getCase(c)!=null){
-				if(j.getCase(c).getEstNoir()==this.getEstNoir()){
-					return false;
+			while(i<c.getAbcisse() || flag){
+				if(p.getCase(new Coordonnees(c.getOrdonnee(),i))==null){
+					i++;
 				}
 				else{
-					return true;
+					flag=false;
 				}
 			}
-		}
-		else{
-			return false;
+			if(i==c.getAbcisse() && p.getCase(c)!=null){
+				if(p.getCase(c).getEstNoir()==this.getEstNoir()){
+					return false;
+				}
+				return true;
+			}
+			return flag;
 		}
 		
+		if(this.getCoordonnees().getAbcisse()==c.getAbcisse() && this.getCoordonnees().getOrdonnee()!=c.getOrdonnee()){
+			
+			while(i<c.getOrdonnee() || flag){
+				if(p.getCase(new Coordonnees(i,c.getAbcisse()))==null){
+					i++;
+				}
+				else{
+					flag=false;
+				}
+			}
+			if(i==c.getOrdonnee() && p.getCase(c)!=null){
+				if(p.getCase(c).getEstNoir()==this.getEstNoir()){
+					return false;
+				}
+				return true;
+			}
+			return flag;
+		}
+		return false;
 	}
 }

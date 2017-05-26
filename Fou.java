7fa39877399aa0d5dd67e14e8f64ee8a863940d5
synchi64 <y.moussan@gmail.com>
2017-05-26 14:43:49 +0200

    Update Fou.java
    lol

--- Fou.java
+++ Fou.java
@@ -115,4 +115,14 @@ public class Fou extends Piece{
 
 		return false;
 	}
+	
+		public String toString(){
+		if(this.getEstNoir()==true){
+			return ("NFou");
+		}
+		if(this.getEstNoir()==false){
+			return ("BFou");
+		}
+		return null;
+	}
 }
